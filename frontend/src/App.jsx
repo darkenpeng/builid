53c1b0e0b0a077e9e54b616067d604f304544da9
HamZik <b1y9mn@gmail.com>
2023-07-02 15:24:00 +0900

    feat: navbar 생성 및 라우팅 #12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './pages/RootLayout';
 import MainAndShop from './pages/MainAndShop';
+import Board from './pages/Board';
+import About from './pages/About';
+import MyPage from './pages/MyPage';
+import Cart from './pages/Cart';
 
 const router = createBrowserRouter([
   {
@@ -8,6 +12,10 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { path: '/', element: <MainAndShop /> },
+      { path: '/board', element: <Board /> },
+      { path: '/about', element: <About /> },
+      { path: '/mypage', element: <MyPage /> },
+      { path: '/cart', element: <Cart /> },
     ]
   },
 ]);
--- frontend/src/components/Layout/Navigation.jsx
+++ frontend/src/components/Layout/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavigationCartButton from './NavigationCartButton';
-
+import { Link } from 'react-router-dom';
 
 const Navigation = ({ showMainCartHandler }) => {
 
@@ -8,6 +8,11 @@ const Navigation = ({ showMainCartHandler }) => {
     <>
       <header>
         <h1>GreekZik</h1>
+        <Link to='/'>그릭요거트</Link>
+        <Link to='/board'>게시판</Link>
+        <Link to='/about'>소개</Link>
+        <Link to='/mypage'>내 정보</Link>
+        <Link to='/cart'>장바구니</Link>
         <NavigationCartButton onClick={showMainCartHandler} />
       </header>
     </>
